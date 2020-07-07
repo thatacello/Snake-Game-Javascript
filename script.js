70fc71add7ad6461c66c0b056f6ac2564e5e133b
THAIS CARDOSO DE FARIAS <farias.thais@uol.com.br>
2020-07-07 19:31:57 -0300

    Criando a cobrinha

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32; //é o tamanho de cada pixel
+let snake = [];
+snake[0] ={
+    x: 8 * box,
+    y: 8 * box
+}
 
 function criarBG(){
-    context.fillStyle = "lightpink";
+    context.fillStyle = "black";
     context.fillRect(0, 0, 16 * box, 16 * box); //desenha o retângulo onde entará o jogo
 }
 
+function criarCobrinha(){
+    for(i=0; i < snake.length; i++){
+        context.fillStyle = "pink";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+}
+
 criarBG();
+criarCobrinha();
